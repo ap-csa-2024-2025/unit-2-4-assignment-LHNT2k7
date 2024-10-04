143eb9b47fc10579fd33ecd949099dbb4dcbd14c
LHNT2k7 <ltri9573@gmail.com>
2024-10-04 10:07:14 -0700

    sdfghjk

--- Main.java
+++ Main.java
@@ -2,6 +2,14 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // write your code here
+    System.out.println(firstLastN(cream,butter,3));
+  }
+  public static String firstLastN(String word, String word2, int n)
+  {
+      String output = new String();
+      String FirstN = word2.substring(0,n);
+      int indexOfNthtoLast = word.length() - n;
+      String LastN = word.substring(indexOfNthtoLast);
+      return FirstN + LastN;
   }
 }
